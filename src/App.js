42ef77cb9e3e665d73419599b5e00178cea5e19c
Lucy <lucybellott@gmail.com>
2024-01-02 15:22:37 -0500

    types

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  
-  
-  
-  
-  
+  // Check for any unterminated string literals or syntax issues around here
+
   return (
     <div className="App">
       <h1>Politics for dummies</h1>
+      <h2>Types of politicians</h2>
+      <ul>
+        <li>Councilor</li>
+        <li>Commissioner</li>
+        <li>Mayor</li>
+        <li>Governor</li>
+        <li>Representative</li>
+        <li>Senator</li>
+        <li>President</li>
+        <li>Congressman</li>
+      </ul>
     </div>
   );
 }
